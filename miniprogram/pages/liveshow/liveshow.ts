4c59a2af76e34bbf4b2c0d323fc44aa0c23c4670
helloocc <xixizxx@gmail.com>
2022-12-02 23:52:17 +0800

    liveshow page demo.

---
+++ miniprogram/pages/liveshow/liveshow.ts
@@ -0,0 +1,70 @@
+// pages/index/live_info.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    liveshow: [
+      { "id": 1, "performers": "111", "site": "222", "date": "2023.01.01", "url": "www" },
+      { "id": 2, "performers": "aaa", "site": "bbb", "date": "2023.01.01", "url": "http" },
+      { "id": 3, "performers": "ccc", "site": "ddd", "date": "2023.01.02", "url": "www" },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
