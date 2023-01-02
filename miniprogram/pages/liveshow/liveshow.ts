665a29ef6954b867613edf4d388e64deb5d2263c
helloocc <xixizxx@gmail.com>
2023-01-02 23:34:44 +0800

    Implement search keyword.

--- miniprogram/pages/liveshow/liveshow.ts
+++ miniprogram/pages/liveshow/liveshow.ts
@@ -1,30 +1,47 @@
 // pages/index/live_info.ts
 import moment from 'moment';
-Page({
 
+const URL = "https://www.chenyunfei.cn"
+const LIVESHOW = "/liveshow_list"
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-    showData: []
+    showData: [],
+    searchKey: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
+    this.doQuery()
+  },
+
+  flushData: function (e: any) {
+    this.setData({
+      searchKey: e.detail.value
+    })
+    this.doQuery()
+    wx.showToast({
+      title: this.data.searchKey + "搜索成功"
+    });
+  },
+
+  doQuery() {
     let that = this
     wx.request({
-      url: 'https://www.chenyunfei.cn/liveshow_list',
-      data: {},
+      url: URL + LIVESHOW,
+      data: { 'query': that.data.searchKey },
       header: {
         'content-type': 'application/json'
       },
       success(res: any) {
-        console.log(res.data)
         for (let month_data of res.data.data) {
           for (let item of month_data.month_show) {
-            item.show_day = moment(item.show_time * 1000).format('YYYY.MM.DD')
+            item.show_day = moment(item.show_time * 1000).format('MM.DD')
             item.show_time = moment(item.show_time * 1000).format('YYYY.MM.DD HH:mm')
           }
         }
@@ -34,7 +51,6 @@ Page({
         console.log(res.data)
       }
     })
-
   },
 
   /**
